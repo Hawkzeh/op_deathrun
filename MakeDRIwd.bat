bf7ea147e439cba7d57e19f8bec001d00b0aeede
Hawkzeh <Hawkzeh@live.com.au>
2014-02-19 22:29:40 +1030

    Commit #6 - Final

---
+++ MakeDRIwd.bat
@@ -0,0 +1,8 @@
+del dr.iwd
+
+7za a -r -tzip dr.iwd images
+7za a -r -tzip dr.iwd weapons
+7za a -r -tzip dr.iwd deathrun_readme.txt
+7za a -r -tzip dr.iwd dr_sprays.cfg
+
+pause
---
+++ MakeDrsndIwd.bat
@@ -0,0 +1,7 @@
+del drsnd.iwd
+
+7za a -r -tzip drsnd.iwd sound
+7za a -r -tzip drsnd.iwd dr_songs.cfg
+7za a -r -tzip drsnd.iwd maptrax_dvars.cfg
+
+pause
